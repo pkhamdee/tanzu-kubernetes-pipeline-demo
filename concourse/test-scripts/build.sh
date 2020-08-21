744985cd8d194ec769fd40d1897846d9fda908ee
ubuntu <ubuntu@ubuntu-403.haas-403.pez.vmware.com>
2020-08-21 08:00:08 -0700

    add build script

---
+++ concourse/test-scripts/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+set -x
+
+export ROOT_FOLDER=$( pwd )
+
+M2_HOME="${HOME}/.m2"
+M2_CACHE="${ROOT_FOLDER}/maven"
+
+echo "Generating symbolic links for caches"
+
+[[ -d "${M2_CACHE}" && ! -d "${M2_HOME}" ]] && ln -s "${M2_CACHE}" "${M2_HOME}"
+
+mvn clean install
