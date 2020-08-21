2764362af5a0cf7eaf782e7316ed0a4eea8e8f9b
ubuntu <ubuntu@ubuntu-403.haas-403.pez.vmware.com>
2020-08-21 09:46:27 -0700

    add cache to tests

--- concourse/test-scripts/unit-test.sh
+++ concourse/test-scripts/unit-test.sh
@@ -1,7 +1,16 @@
 #! /bin/bash
 
+export ROOT_FOLDER=$( pwd )
+
+M2_HOME="${HOME}/.m2"
+M2_CACHE="${ROOT_FOLDER}/maven"
+
+echo "Generating symbolic links for caches"
+
+[[ -d "${M2_CACHE}" && ! -d "${M2_HOME}" ]] && ln -s "${M2_CACHE}" "${M2_HOME}"
+
 cd spring-petclinic-testing-branch
 
-#mvn test
+mvn test
 
 exit 0
